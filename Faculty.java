0b15fb2e1555f84a91abe704597cd2b95dc89a29
Tristanityyyyy <145412794+Tristanityyyyy@users.noreply.github.com>
2023-10-24 13:01:02 +0800

    Add files via upload

---
+++ Faculty.java
@@ -0,0 +1,16 @@
+package package1;
+
+class Faculty extends Employee {
+	private boolean status;
+	
+	public boolean isRegular() {
+		
+		if(a == 'Y') {
+			this.status = true;
+		}
+		else {
+			this.status = false;
+		}
+		return status;
+	}
+}
